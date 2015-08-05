17d491b72c44b01d58544b7f04f799ca59606703
Pramod Kumar <pramod493@gmail.com>
2015-08-05 11:57:17 -0400

    Updated PlayGoCore header file

--- Core/playgocore.h
+++ Core/playgocore.h
@@ -1,17 +1,36 @@
 #ifndef __PLAYGO_CORE_H__
 #define __PLAYGO_CORE_H__
 
+#include "asmopencv.h"
+#include "clipper.hpp"
+#include "cdiboxdebugdraw.h"
+#include "grabcut.h"
+#include "ramerdouglaspeucker.h"
+#include "filesystemwatcher.h"
+#include "pdollarrecognizer.h"
+
 #include "commonfunctions.h"
 #include "abstractmodelitem.h"
+
 #include "point2dpt.h"
 #include "stroke.h"
 #include "penstroke.h"
-#include "image.h"
+#include "pixmap.h"
 #include "polygon2d.h"
+#include "searchresult.h"
+#include "SearchAPI.h"
+
 #include "component.h"
 #include "assembly.h"
+
 #include "page.h"
 #include "playgo.h"
+
+#include "physicsbody.h"
+#include "physicsjoint.h"
+#include "physicsshape.h"
+#include "forceitem.h"
+
 #include "searchmanager.h"
 #include "physicsmanager.h"
 #include "ramerdouglaspeucker.h"
