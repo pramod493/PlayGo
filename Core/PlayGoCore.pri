8b8838f7e9a080543caff864ce609bd6dc42cea6
Pramod Kumar <pramod493@gmail.com>
2015-08-21 01:29:09 -0400

    Enable qCustomPlot

--- Core/PlayGoCore.pri
+++ Core/PlayGoCore.pri
@@ -3,7 +3,8 @@ include ($$PWD/../poly2tri/poly2tri.pri)
 include ($$PWD/../Physics/QBox2D.pri)
 include ($$PWD/../EdgeDetect/Edge Detect.pri)
 # Enable when adding plots
-#include ($$PWD/../qCustomPlot/qCustomPlot.pri)
+include ($$PWD/../qCustomPlot/qCustomPlot.pri)
+
 include ($$PWD/../QsLog/QsLog.pri)
 
 #DEFINES += CDI_DEBUG_DRAW_SHAPE
@@ -11,76 +12,76 @@ include ($$PWD/../QsLog/QsLog.pri)
 INCLUDEPATH += $$PWD
 
 HEADERS += $$PWD/clipper.hpp \
-    $$PWD/grabcut.h \
-    $$PWD/cdiboxdebugdraw.h \
-    $$PWD/forceitem.h \
-    $$PWD/penstroke.h \
-    $$PWD/scenebutton.h \
-    $$PWD/cdcontactlistener.h \
-    $$PWD/material.h
+	$$PWD/grabcut.h \
+	$$PWD/cdiboxdebugdraw.h \
+	$$PWD/forceitem.h \
+	$$PWD/penstroke.h \
+	$$PWD/scenebutton.h \
+	$$PWD/cdcontactlistener.h \
+	$$PWD/material.h
 SOURCES += $$PWD/clipper.cpp \
-    $$PWD/grabcut.cpp \
-    $$PWD/forceitem.cpp \
-    $$PWD/penstroke.cpp \
-    $$PWD/scenebutton.cpp \
-    $$PWD/cdcontactlistener.cpp \
-    $$PWD/material.cpp
+	$$PWD/grabcut.cpp \
+	$$PWD/forceitem.cpp \
+	$$PWD/penstroke.cpp \
+	$$PWD/scenebutton.cpp \
+	$$PWD/cdcontactlistener.cpp \
+	$$PWD/material.cpp
 
 HEADERS += \
-    $$PWD/ramerdouglaspeucker.h \
-    $$PWD/asmopencv.h \
-    $$PWD/commonfunctions.h \
-    $$PWD/abstractmodelitem.h \
-    $$PWD/cdi2qtwrapper.h \
-    $$PWD/point2dpt.h 
-	
-SOURCES += \
-    $$PWD/ramerdouglaspeucker.cpp  \
-    $$PWD/commonfunctions.cpp  \
-    $$PWD/abstractmodelitem.cpp  \
-    $$PWD/cdi2qtwrapper.cpp  \
-    $$PWD/point2dpt.cpp  
-	
-HEADERS += \
-    $$PWD/physicsbody.h \
-    $$PWD/physicsjoint.h \
-    $$PWD/physicsshape.h \
-    $$PWD/searchresult.h 
-	
-SOURCES += \
-    $$PWD/physicsbody.cpp  \
-    $$PWD/physicsjoint.cpp  \
-    $$PWD/physicsshape.cpp  \
-    $$PWD/searchresult.cpp 
-	
-HEADERS += \
-    $$PWD/stroke.h \
-    $$PWD/pixmap.h \
-    $$PWD/polygon2d.h \
-    $$PWD/component.h \
-    $$PWD/assembly.h \
-    $$PWD/page.h \
-    $$PWD/playgo.h 
-	
-SOURCES += \
-    $$PWD/stroke.cpp  \
-    $$PWD/pixmap.cpp  \
-    $$PWD/polygon2d.cpp  \
-    $$PWD/component.cpp  \
-    $$PWD/assembly.cpp  \
-    $$PWD/page.cpp  \
-    $$PWD/playgo.cpp
-	
-HEADERS += \
-    $$PWD/fileio.h \
-    $$PWD/physicsmanager.h \
-    $$PWD/searchmanager.h \
-    $$PWD/playgocore.h \
-    $$PWD/filesystemwatcher.h
+	$$PWD/ramerdouglaspeucker.h \
+	$$PWD/asmopencv.h \
+	$$PWD/commonfunctions.h \
+	$$PWD/abstractmodelitem.h \
+	$$PWD/cdi2qtwrapper.h \
+	$$PWD/point2dpt.h
 
 SOURCES += \
-    $$PWD/fileio.cpp  \
-    $$PWD/physicsmanager.cpp  \
-    $$PWD/searchmanager.cpp \
-    $$PWD/filesystemwatcher.cpp
+	$$PWD/ramerdouglaspeucker.cpp  \
+	$$PWD/commonfunctions.cpp  \
+	$$PWD/abstractmodelitem.cpp  \
+	$$PWD/cdi2qtwrapper.cpp  \
+	$$PWD/point2dpt.cpp
+
+HEADERS += \
+	$$PWD/physicsbody.h \
+	$$PWD/physicsjoint.h \
+	$$PWD/physicsshape.h \
+	$$PWD/searchresult.h
+
+SOURCES += \
+	$$PWD/physicsbody.cpp  \
+	$$PWD/physicsjoint.cpp  \
+	$$PWD/physicsshape.cpp  \
+	$$PWD/searchresult.cpp
+
+HEADERS += \
+	$$PWD/stroke.h \
+	$$PWD/pixmap.h \
+	$$PWD/polygon2d.h \
+	$$PWD/component.h \
+	$$PWD/assembly.h \
+	$$PWD/page.h \
+	$$PWD/playgo.h
+
+SOURCES += \
+	$$PWD/stroke.cpp  \
+	$$PWD/pixmap.cpp  \
+	$$PWD/polygon2d.cpp  \
+	$$PWD/component.cpp  \
+	$$PWD/assembly.cpp  \
+	$$PWD/page.cpp  \
+	$$PWD/playgo.cpp
+
+HEADERS += \
+	$$PWD/fileio.h \
+	$$PWD/physicsmanager.h \
+	$$PWD/searchmanager.h \
+	$$PWD/playgocore.h \
+	$$PWD/filesystemwatcher.h
+
+SOURCES += \
+	$$PWD/fileio.cpp  \
+	$$PWD/physicsmanager.cpp  \
+	$$PWD/searchmanager.cpp \
+	$$PWD/filesystemwatcher.cpp
 
--- main.cpp
+++ main.cpp
@@ -28,6 +28,7 @@
 #include <qtuio.h>
 #endif //Q_OS_LINUX
 
+#include "qCustomPlot/qcustomplot.h"
 
 int main(int argc, char *argv[])
 {
@@ -36,7 +37,7 @@ int main(int argc, char *argv[])
 	/****************************************************************
 	 * Set up themes
 	 * **************************************************************/
-    QStringList list = QStyleFactory::keys();
+	QStringList list = QStyleFactory::keys();
 #ifdef Q_OS_LINUX
 	QStyle* style = QStyleFactory::create("Windows");   // Looks more theme independent
 	if (style)	app.setStyle(style);
@@ -55,7 +56,7 @@ int main(int argc, char *argv[])
 	QString qslog_time_format("yyyy-MM-ddThh-mm-ss");
 	QString logfilename = QDateTime::currentDateTime().toString(qslog_time_format);
 	logfilename += QString(".log");
-	// We can also ask for the log file name to be one of the arguments or 
+	// We can also ask for the log file name to be one of the arguments or
 	// pop-up text box for book-keeping the user information
 	logfilename = "log.txt";
 	// init the logging mechanism
@@ -82,12 +83,79 @@ int main(int argc, char *argv[])
 	}
 
 	/****************************************************************
+	 * Test QCustomPlot basic plotting
+	 * (Only for testing purposes)
+	 * **************************************************************/
+	if (false) {
+
+		QCustomPlot* customPlot = new QCustomPlot();
+		// Plot 01
+		{
+			// generate some data:
+			QVector<double> x(101), y(101); // initialize with entries 0..100
+			for (int i=0; i<101; ++i)
+			{
+				x[i] = i/50.0 - 1; // x goes from -1 to 1
+				y[i] = x[i]*x[i]; // let's plot a quadratic function
+			}
+			// create graph and assign data to it:
+			customPlot->addGraph();
+			customPlot->graph(0)->setData(x, y);
+			// give the axes some labels:
+			customPlot->xAxis->setLabel("x");
+			customPlot->yAxis->setLabel("y");
+			// set axes ranges, so we see all data:
+			customPlot->xAxis->setRange(-1, 1);
+			customPlot->yAxis->setRange(0, 1);
+			customPlot->replot();
+			customPlot->show();
+		}
+		// Plot 02
+		{
+			// add two new graphs and set their look:
+			customPlot->addGraph();
+			customPlot->graph(0)->setPen(QPen(Qt::blue)); // line color blue for first graph
+			customPlot->graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20))); // first graph will be filled with translucent blue
+			customPlot->addGraph();
+			customPlot->graph(1)->setPen(QPen(Qt::red)); // line color red for second graph
+			// generate some points of data (y0 for first, y1 for second graph):
+			QVector<double> x(250), y0(250), y1(250);
+			for (int i=0; i<250; ++i)
+			{
+				x[i] = i;
+				y0[i] = exp(-i/150.0)*cos(i/10.0); // exponentially decaying cosine
+				y1[i] = exp(-i/150.0); // exponential envelope
+			}
+			// configure right and top axis to show ticks but no labels:
+			// (see QCPAxisRect::setupFullAxesBox for a quicker method to do this)
+			customPlot->xAxis2->setVisible(true);
+			customPlot->xAxis2->setTickLabels(false);
+			customPlot->yAxis2->setVisible(true);
+			customPlot->yAxis2->setTickLabels(false);
+			// make left and bottom axes always transfer their ranges to right and top axes:
+			QObject::connect(customPlot->xAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->xAxis2, SLOT(setRange(QCPRange)));
+			QObject::connect(customPlot->yAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->yAxis2, SLOT(setRange(QCPRange)));
+			// pass data points to graphs:
+			customPlot->graph(0)->setData(x, y0);
+			customPlot->graph(1)->setData(x, y1);
+			// let the ranges scale themselves so graph 0 fits perfectly in the visible area:
+			customPlot->graph(0)->rescaleAxes();
+			// same thing for graph 1, but only enlarge ranges (in case graph 1 is smaller than graph 0):
+			customPlot->graph(1)->rescaleAxes(true);
+			// Note: we could have also just called customPlot->rescaleAxes(); instead
+			// Allow user to drag axis ranges with mouse, zoom with mouse wheel and select graphs by clicking:
+			customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
+		}
+		return app.exec();
+	}
+
+	/****************************************************************
 	 * Launch PlayGo main window
 	 * **************************************************************/
 	CDI::CDIWindow *window =  new CDI::CDIWindow();
 	// Triggered when stylus enters/leaves the device proximity
 	QObject::connect(&app, SIGNAL(signalStylusEnter()),
-                     window, SLOT(onStylusEnter()));
+					 window, SLOT(onStylusEnter()));
 
 	QObject::connect(&app, SIGNAL(signalStylusLeave()),
 					 window, SLOT(onStylusLeave()));
@@ -100,10 +168,12 @@ int main(int argc, char *argv[])
 	 * Initialize TUIO and attach to the sketch view
 	 * **************************************************************/
 #ifdef Q_OS_LINUX
-	if (true)	// Set to true if TUIO is enabled
+	bool tuio_enabled = false;
+	if (tuio_enabled)	// Set to true if TUIO is enabled
 	{
+		// default port is 3333. Use multiple ports for multi-screen
 		QTuio* tuio = new QTuio(window->sketchView);
-		tuio->run();	// port: 3333
+		tuio->run();
 	}
 #endif //Q_OS_LINUX
 	/****************************************************************
